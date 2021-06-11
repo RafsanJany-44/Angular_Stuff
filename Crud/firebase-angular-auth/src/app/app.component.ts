f9072873042b914c47eaf3e54cc12b18e4de703a
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-06-12 02:02:08 +0600

    up

--- Crud/firebase-angular-auth/src/app/app.component.ts
+++ Crud/firebase-angular-auth/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FirebaseService} from './services/firebase.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-angular-auth';
+  isSignedIn = false;
+  constructor(public firebaseService: FirebaseService){}
+  ngOnInit(){
+    if(localStorage.getItem('user')!=null)
+    this.isSignedIn=true
+    else
+    this.isSignedIn=false
+  }
+
+  async onSignup(email:string,password:string){
+    await this.firebaseService.signup(email,password)
+    if(this.firebaseService.isLoggedIn)
+    this.isSignedIn=true
+  }
+
+  async onSignin(email:string,password:string){
+    await this.firebaseService.signup(email,password)
+    if(this.firebaseService.isLoggedIn)
+    this.isSignedIn=true
+  }
+
+  handleLogin(){}
 }
--- Crud/firebase-angular-auth/src/app/app.module.ts
+++ Crud/firebase-angular-auth/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AngularFireModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAQkzi4fGlWX0x09x_WrFnj9K3UTjuk-EM",
+      authDomain: "fir-anguler-auth.firebaseapp.com",
+      projectId: "fir-anguler-auth",
+      storageBucket: "fir-anguler-auth.appspot.com",
+      messagingSenderId: "749272896585",
+      appId: "1:749272896585:web:1637127be4fb3a00041b98"
+    })
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
